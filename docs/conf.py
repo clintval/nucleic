7cb1a2f654062be2a50552c7bcbc982ccc7f56cc
clintval <valentine.clint@gmail.com>
2018-11-07 23:03:55 -0800

    v0.5.0

--- docs/conf.py
+++ docs/conf.py
@@ -19,9 +19,9 @@ copyright = '2018, TwinStrand Biosciences'
 author = 'clintval'
 
 # The short X.Y version
-version = '0.1'
+version = '0.5'
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.5.0'
 
 # -- General configuration ---------------------------------------------------
 
